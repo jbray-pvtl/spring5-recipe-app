e2f976f503eb441c98a295734662efaa3660d5d1
jbrayjr2006 <jbrayjr2006@gmail.com>
2018-05-01 11:37:16 -0500

    Added styling to UI

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,27 @@
 package guru.springframework.controllers;
 
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        Long categoryId = categoryRepository.findByDescription("Italian").get().getId();
+        Long unitOfMeasureId = unitOfMeasureRepository.findByDescription("Teaspoon").get().getId();
+        System.out.println("Category ID is " + categoryId);
+        System.out.println("Unit of Measure ID is " + unitOfMeasureId);
         return "index";
     }
 }
