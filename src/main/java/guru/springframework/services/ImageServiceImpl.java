9b77c30860d7902252303d81e8a7b9e8b080085f
jbrayjr2006 <jbrayjr2006@gmail.com>
2018-05-31 11:02:40 -0500

    Added image upload and persistence code

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,13 +1,41 @@
 package guru.springframework.services;
 
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 public class ImageServiceImpl implements ImageService {
 
+    RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long id, MultipartFile image) {
         log.info("Saving uploaded file...");
+
+        Recipe recipe = recipeRepository.findById(id).get();
+
+        try {
+            Byte[] byteObjects = new Byte[image.getBytes().length];
+
+            int i = 0;
+
+            for(byte b: image.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+
+        } catch(IOException ioe) {
+            log.error("Error occurred while attempting to save image file: " + ioe.getLocalizedMessage());
+        }
     }
 }
--- src/test/java/guru/springframework/services/ImageServiceTest.java
+++ src/test/java/guru/springframework/services/ImageServiceTest.java
@@ -1,20 +1,58 @@
 package guru.springframework.services;
 
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class ImageServiceTest {
+
+    @Mock
+    RecipeRepository mockRecipeRepository;
+
+    ImageService imageService;
 
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
+        imageService = new ImageServiceImpl(mockRecipeRepository);
+    }
+
+    @Test
+    void shouldSaveImageFileTest() throws Exception {
+        //Given
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("file", "my-file.txt", "text/plain","Spring Framework".getBytes());
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        //When
+        when(mockRecipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+        imageService.saveImageFile(id, multipartFile);
+
+        //Then
+        verify(mockRecipeRepository, times(1)).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
     }
 }
